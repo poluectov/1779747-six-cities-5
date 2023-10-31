17799f0eabcdc83d2ed53df543ae90e230e98cb0
poluectov <poluectov@list.ru>
2023-10-31 11:47:07 +0300

    добавил набор тестовых данных для json-server

---
+++ src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,13 @@
+export type MockServerData = {
+    titles: string[];
+    descriptions: string[];
+    cities: string[];
+    previewPhotos: string[];
+    photos: string[];
+    types: string;
+    facilities: string[];
+    users: string[];
+    emails: string[];
+    passwords: string[];
+    avatars: string[];
+  };
