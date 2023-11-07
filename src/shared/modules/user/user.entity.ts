e4595d37b068a2947fc115a8fdbb2b2565e3c766
poluectov <poluectov@list.ru>
2023-11-08 00:36:13 +0300

    Сущность User

---
+++ src/shared/modules/user/user.entity.ts
@@ -0,0 +1,48 @@
+import { defaultClasses, getModelForClass, prop, modelOptions } from '@typegoose/typegoose';
+import { User, UserType } from '../../types/index.js';
+import { createSHA256 } from '../../helpers/index.js';
+
+export interface UserEntity extends defaultClasses.Base {}
+
+@modelOptions({
+    schemaOptions: {
+      collection: 'users'
+    }
+  })
+
+export class UserEntity extends defaultClasses.TimeStamps implements User {
+  @prop({ required: true, default: '' })
+  public name: string;
+
+  @prop({ unique: true, required: true })
+  public email: string;
+
+  @prop({ required: false, default: '' })
+  public avatar: string;
+
+  @prop({ required: true, default: '' })
+  public userType: UserType;
+
+  @prop({ required: true, default: '' })
+  private password?: string;
+
+  constructor(userData: User) {
+    super();
+
+    this.email = userData.email;
+    this.avatar = userData.avatar;
+    this.name = userData.name;
+    this.userType = userData.userType
+    
+  }
+
+  public setPassword(password: string, salt: string) {
+    this.password = createSHA256(password, salt);
+  }
+
+  public getPassword() {
+    return this.password;
+  }
+}
+
+export const UserModel = getModelForClass(UserEntity);
