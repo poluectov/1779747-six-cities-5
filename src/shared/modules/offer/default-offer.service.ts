e86e9b0fdf172ee7481e897e25624feff7722a12
poluectov <poluectov@list.ru>
2023-11-11 21:41:34 +0300

    расширил интерфейсы UserService, OfferService

--- src/shared/modules/offer/default-offer.service.ts
+++ src/shared/modules/offer/default-offer.service.ts
@@ -1,10 +1,12 @@
 import { inject, injectable } from 'inversify';
 import { OfferService } from './offer-service.interface.js';
-import { Component } from '../../types/index.js';
+import { Component, SortType} from '../../types/index.js';
 import { Logger } from '../../libs/logger/index.js';
 import { DocumentType, types } from '@typegoose/typegoose';
 import { OfferEntity } from './offer.entity.js';
 import { CreateOfferDto } from './dto/create-offer.dto.js';
+import { UpdateOfferDto } from './dto/update-offer.dto.js';
+import { DEFAULT_OFFER_COUNT, DEFAULT_PREMIUM_COUNT } from './offer.constant.js';
 
 @injectable()
 export class DefaultOfferService implements OfferService {
@@ -21,6 +23,82 @@ export class DefaultOfferService implements OfferService {
   }
 
   public async findById(offerId: string): Promise<DocumentType<OfferEntity> | null> {
-    return this.offerModel.findById(offerId).exec();
+    return this.offerModel
+    .findById(offerId)
+    .exec();
   }
+
+  public async find(): Promise<DocumentType<OfferEntity>[]> {
+      return await this.offerModel
+      .aggregate([
+        {
+          $lookup: {
+            from: 'comments',
+            let: { offerId: '$_id'},
+            pipeline: [
+              { $match: { $expr: { $in: [ '$$offerId', '$comments' ] } } },
+              { $project: { _id: 1 }}
+            ],
+            as: 'comments'
+          },
+        },
+        { $addFields:
+            { id: { $toString: '$_id' }, comments: { $size: '$comments'} }
+        },
+        { $unset: 'comments' },
+        { $limit: DEFAULT_OFFER_COUNT },
+        { $sort: { comments: SortType.Down } }
+      ])
+      .exec();
+  }
+
+
+  public async updateById(offerId: string, dto: UpdateOfferDto): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndUpdate(offerId, dto, {new: true})
+      .populate(['userId'])
+      .exec();
+  }
+
+  public async findPremiumByCity(city: string, isPremium: boolean = true): Promise<DocumentType<OfferEntity>[]> {
+    const limit = DEFAULT_PREMIUM_COUNT;
+    return this.offerModel
+      .find({ city, isPremium }, {}, { limit })
+      .populate([ 'userId' ])
+      .exec()
+  }
+  
+  public async findFavorites(userId: string, isFavorite: boolean = true): Promise<DocumentType<OfferEntity>[]> {
+        return this.offerModel
+          .find({ userId, isFavorite })
+          .populate([ 'userId' ])
+          .exec();
+     }
+
+  public async setFavoriteStatus(offerId: string, dto: UpdateOfferDto): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndUpdate(offerId, dto, { new: true })
+      .populate([ 'userId' ])
+      .exec();
+  }
+
+  public async deleteById(offerId: string): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndDelete(offerId)
+      .exec();
+  }
+
+
+  public async incCommentCount(offerId: string): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndUpdate(offerId, {'$inc': {
+        commentCount: 1,
+      }}).exec();
+  }
+
+  public async exists(documentId: string): Promise<boolean> {
+    return (await this.offerModel
+      .exists({_id: documentId})) !== null;
+  }
+
 }
