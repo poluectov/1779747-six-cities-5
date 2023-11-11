909fed5882698599b546aca914d158facb67a5c6
poluectov <poluectov@list.ru>
2023-11-11 13:05:52 +0300

    добавил сущность comment

---
+++ src/shared/modules/comment/default-comment.service.ts
@@ -0,0 +1,40 @@
+import { inject, injectable } from 'inversify';
+import { CommentService } from './comment-service.interface.js';
+import { Component, SortType } from '../../types/index.js';
+import { DocumentType, types } from '@typegoose/typegoose';
+import { CommentEntity } from './comment.entity.js';
+import { CreateCommentDto } from './dto/create-comment.dto.js';
+import { Logger } from '../../libs/logger/index.js';
+import { DEFAULT_COMMENT_COUNT } from './comment.constant.js';
+
+@injectable()
+export class DefaultCommentService implements CommentService {
+  constructor(
+    @inject(Component.Logger) private readonly logger: Logger,
+    @inject(Component.CommentModel) private readonly commentModel: types.ModelType<CommentEntity>
+  ) {}
+
+  public async create(dto: CreateCommentDto): Promise<DocumentType<CommentEntity>> {
+    const comment = await this.commentModel.create(dto);
+    this.logger.info(`New comment created: ${dto.offerId}`);
+    
+    return comment.populate('userId');
+  }
+
+  public async findByOfferId(offerId: string, count?: number): Promise<DocumentType<CommentEntity>[]> {
+    const limit = !count || count > DEFAULT_COMMENT_COUNT ? DEFAULT_COMMENT_COUNT : count;
+    return this.commentModel
+      .find({offers: offerId}, {}, {limit})
+      .Sort({createAt: SortType.Down})
+      .populate(['userId', 'offerId'])
+      .exec();
+  }
+
+  public async deleteByOfferId(offerId: string): Promise<number> {
+    const result = await this.commentModel
+      .deleteMany({offerId})
+      .exec();
+
+    return result.deletedCount;
+  }
+}
