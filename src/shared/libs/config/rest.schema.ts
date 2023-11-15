c01a280e572bfb7adca09ecfacde7121ec0f0cf1
poluectov <poluectov@list.ru>
2023-11-15 21:55:23 +0300

    Дорожные работы

--- src/shared/libs/config/rest.schema.ts
+++ src/shared/libs/config/rest.schema.ts
@@ -11,6 +11,7 @@ export type RestSchema = {
   DB_PASSWORD: string;
   DB_PORT: string;
   DB_NAME: string;
+  UPLOAD_DIRECTORY: string;
 }
 
 export const configRestSchema = convict<RestSchema>({
@@ -56,5 +57,11 @@ export const configRestSchema = convict<RestSchema>({
     format: String,
     env: 'DB_NAME',
     default: 'six-cities'
+  },
+  UPLOAD_DIRECTORY: {
+    doc: 'Directory for upload files',
+    format: String,
+    env: 'UPLOAD_DIRECTORY',
+    default: null
   },
 });
--- src/shared/modules/offer/dto/update-offer.dto.ts
+++ src/shared/modules/offer/dto/update-offer.dto.ts
@@ -1,69 +1,83 @@
 import { CityType, OfferType, FacilitiesType } from '../../../types/index.js';
 import { UpdateOfferMessage } from './update-offer.messages.js';
 import {
-  IsArray, IsDateString, MaxLength, MinLength, ArrayMinSize, ArrayMaxSize,
+  IsOptional, IsArray, IsDateString, MaxLength, MinLength, ArrayMinSize, ArrayMaxSize,
   IsBoolean, IsInt, Min, Max, IsObject, ValidateNested,
 } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class UpdateOfferDto {
+  @IsOptional()
   @MinLength(10, { message: UpdateOfferMessage.title.minLength })
   @MaxLength(100, { message: UpdateOfferMessage.title.maxLength })
   public title: string;
 
+  @IsOptional()
   @MinLength(20, { message: UpdateOfferMessage.description.minLength })
   @MaxLength(1024, { message: UpdateOfferMessage.description.maxLength })
   public description: string;
 
+  @IsOptional()
   @IsDateString({}, { message: UpdateOfferMessage.postDate.invalidFormat })
   public postDate: Date;
 
+  @IsOptional()
   @IsObject()
-  @ValidateNested()
+  @ValidateNested({ message: UpdateOfferMessage.city.invalidFormat })
   @Type(() => String)
   public city: CityType;
 
+  @IsOptional()
   @MaxLength(256, { message: UpdateOfferMessage.previewPhoto.maxLength })
   public previewPhoto: string;
 
+  @IsOptional()
   @IsArray({ message: UpdateOfferMessage.photos.invalidFormat })
   @ArrayMinSize(6, { message: UpdateOfferMessage.photos.ArraySize })
   @ArrayMaxSize(6, { message: UpdateOfferMessage.photos.ArraySize })
   @MaxLength(256, { each: true, message: UpdateOfferMessage.previewPhoto.maxLength })
   public photos: string[];
 
+  @IsOptional()
   @IsBoolean({ message: UpdateOfferMessage.isPremium.IsBoolean })
   public isPremium: boolean;
 
+  @IsOptional()
   @IsBoolean({ message: UpdateOfferMessage.isFavorite.IsBoolean })
   public isFavorite: boolean;
 
+  @IsOptional()
   @IsInt({ message: UpdateOfferMessage.rating.invalidFormat })
   @Min(1, { message: UpdateOfferMessage.rating.minValue })
   @Max(5, { message: UpdateOfferMessage.rating.maxValue })
   public rating: number;
 
+  @IsOptional()
   @IsObject()
-  @ValidateNested()
+  @ValidateNested({ message: UpdateOfferMessage.type.invalidFormat })
   @Type(() => String)
   public type: OfferType;
 
+  @IsOptional()
   @IsInt({ message: UpdateOfferMessage.rooms.invalidFormat })
   @Min(1, { message: UpdateOfferMessage.rooms.minValue })
   @Max(8, { message: UpdateOfferMessage.rooms.maxValue })
   public rooms: number;
 
+  @IsOptional()
   @IsInt({ message: UpdateOfferMessage.guests.invalidFormat })
   @Min(1, { message: UpdateOfferMessage.guests.minValue })
   @Max(10, { message: UpdateOfferMessage.guests.maxValue })
   public guests: number;
 
+  @IsOptional()
   @IsInt({ message: UpdateOfferMessage.price.invalidFormat })
   @Min(100, { message: UpdateOfferMessage.price.minValue })
   @Max(100000, { message: UpdateOfferMessage.price.maxValue })
   public price: number;
 
 
+  @IsOptional()
   @IsArray({ message: UpdateOfferMessage.facilities.invalidFormat })
   @ArrayMinSize(1, { message: UpdateOfferMessage.facilities.ArrayMinSize })
   @ArrayMaxSize(7, { message: UpdateOfferMessage.facilities.ArrayMaxSize })
@@ -72,6 +86,7 @@ export class UpdateOfferDto {
   @Type(() => String)
   public facilities?: FacilitiesType[];
 
+  @IsOptional()
   @IsObject()
   @ValidateNested()
   @Type(() => String)
