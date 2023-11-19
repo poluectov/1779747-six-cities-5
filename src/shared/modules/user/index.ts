3bb49c02f846752262ae8c5b2f442ca681b1e3ff
poluectov <poluectov@list.ru>
2023-11-19 22:03:40 +0300

    модуль Auth

--- src/shared/modules/user/index.ts
+++ src/shared/modules/user/index.ts
@@ -1,5 +1,7 @@
 export * from './user.entity.js';
+export * from './user-service.interface.js';
 export * from './dto/create-user.dto.js';
+export * from './dto/login-user.dto.js';
 export * from './default-user.service.js';
 export * from './user.container.js';
 export * from './user.controller.js';
--- src/shared/types/component.enum.ts
+++ src/shared/types/component.enum.ts
@@ -12,5 +12,7 @@ export const Component = {
   ExceptionFilter: Symbol.for('ExceptionFilter'),
   UserController: Symbol.for('UserController'),
   OfferController: Symbol.for('OfferController'),
-  CommentController: Symbol.for('CommentController')
+  CommentController: Symbol.for('CommentController'),
+  AuthService: Symbol.for('AuthService'),
+  AuthExceptionFilter: Symbol.for('AuthExceptionFilter'),
 } as const;
