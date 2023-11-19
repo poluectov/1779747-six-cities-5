a70a10a6f968c4a960d63efdcb0a2750d949bf0f
poluectov <poluectov@list.ru>
2023-11-19 22:16:54 +0300

    ParseTokenMiddleware

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -7,6 +7,7 @@ import { DatabaseClient } from '../shared/libs/database-client/index.js';
 import { getMongoURI } from '../shared/helpers/index.js';
 // import { UserModel } from '../shared/modules/user/user.entity.js';
 import { Controller, ExceptionFilter } from '../shared/libs/rest/index.js';
+import { ParseTokenMiddleware } from '../shared/libs/rest/middleware/parse-token.middleware.js';
 
 
 @injectable()
@@ -50,11 +51,13 @@ export class RestApplication {
   }
 
   private async _initMiddleware() {
+    const authenticateMiddleware = new ParseTokenMiddleware(this.config.get('JWT_SECRET'));
     this.server.use(express.json());
     this.server.use(
       '/upload',
       express.static(this.config.get('UPLOAD_DIRECTORY'))
     );
+    this.server.use(authenticateMiddleware.execute.bind(authenticateMiddleware));
   }
 
   private async _initExceptionFilters() {
